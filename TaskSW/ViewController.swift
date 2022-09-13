45b9d00e245b2fcc33c46765c3dd940af04345fb
sathiyarajk <30854711+sathiyarajk@users.noreply.github.com>
2022-09-13 15:44:53 +0530

    initial commit

---
+++ TaskSW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskSW
+//
+//  Created by Apple on 10/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
